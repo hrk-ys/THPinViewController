b0538c5f9ffaae26e776cd8f8fe37b0f2c05a689
hiroki.yoshifuji <hiroki.yoshifuji@dena.com>
2014-10-23 10:10:19 +0900

    数字のボタンのデザイン変更

--- THPinViewController/THPinNumButton.m
+++ THPinViewController/THPinNumButton.m
@@ -31,7 +31,7 @@
         _number = number;
         _letters = letters;
         
-        self.layer.cornerRadius = [[self class] diameter] / 2.0f;
+        self.layer.cornerRadius = [[self class] diameter] / 2.0f * 0.5f;
         self.layer.borderWidth = 1.0f;
         
         UIView *contentView = [[UIView alloc] init];
